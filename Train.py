e4522e790baf043a2678870d4df59e63dc31994b
ImJustHenry <65806328+ImJustHenry@users.noreply.github.com>
2025-10-06 20:04:34 -0500

    Add Train

---
+++ Train.py
@@ -0,0 +1,22 @@
+# Subproblem: If I fix one car, what is the longest train I can build starting or ending with that car? 
+
+def train():
+    n = int(input())
+    car = []
+    for i in range(n):
+        car.append(int(input()))
+
+    right = [1] * n
+    left = [1] * n
+    total = 0
+    for i in reversed(range(n)):
+        for j in range(i + 1, n):
+            if car[j] > car[i]:
+                right[i] = max(right[i], right[j] + 1)
+            if car[j] < car[i]:
+                left[i] = max(left[i], left[j] + 1)
+        total = max(total, right[i] + left[i] - 1)
+
+    return total
+    
+print(train())
