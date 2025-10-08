3735021047d33f01a32b81a0ae8225486f4ce7fb
ImJustHenry <65806328+ImJustHenry@users.noreply.github.com>
2025-10-08 18:18:15 -0500

    fixed

--- Train.py
+++ Train.py
@@ -1,4 +1,4 @@
-# Subproblem: If I fix one car, what is the longest train I can build starting or ending with that car? 
+# Subproblem: LIS LDS 
 
 def train():
     n = int(input())
@@ -9,14 +9,16 @@ def train():
     right = [1] * n
     left = [1] * n
     total = 0
+
     for i in reversed(range(n)):
         for j in range(i + 1, n):
             if car[j] > car[i]:
-                right[i] = max(right[i], right[j] + 1)
-            if car[j] < car[i]:
                 left[i] = max(left[i], left[j] + 1)
+            if car[j] < car[i]:
+                right[i] = max(right[i], right[j] + 1)
         total = max(total, right[i] + left[i] - 1)
-
+ 
     return total
     
 print(train())
+
