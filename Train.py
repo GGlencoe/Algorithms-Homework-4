f4bd6cc72dd0b1c5cbdcd8f2d1a791fc285a3b09
ImJustHenry <65806328+ImJustHenry@users.noreply.github.com>
2025-10-08 18:21:17 -0500

    nvm

--- Train.py
+++ Train.py
@@ -13,9 +13,9 @@ def train():
     for i in reversed(range(n)):
         for j in range(i + 1, n):
             if car[j] > car[i]:
-                left[i] = max(left[i], left[j] + 1)
-            if car[j] < car[i]:
                 right[i] = max(right[i], right[j] + 1)
+            if car[j] < car[i]:
+                left[i] = max(left[i], left[j] + 1)
         total = max(total, right[i] + left[i] - 1)
  
     return total
